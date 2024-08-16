c692b65165c36aa3346e8a7fb6ea6dc9a967c7a3
ABDELMAWLASASSIOUI <sasiouiabdelmola@gmail.com>
2024-08-16 22:30:34 +0200

    add swagger functionalities of token authoritie interface

---
+++ src/main/java/com/twd/SpringSecurityJWT/Swagger/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.twd.SpringSecurityJWT.Swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Votre API").version("1.0"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));  // Vous pouvez préciser le format du token (par exemple "JWT")
+    }
+}
+
